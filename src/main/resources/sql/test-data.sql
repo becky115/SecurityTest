16715de7ded2253627da1d49b494be375ae69fb9
ejlee <ejlee@narusec.com>
2016-12-26 14:42:52 +0900

    no message

--- src/main/resources/sql/test-data.sql
+++ src/main/resources/sql/test-data.sql
@@ -1,5 +1,3 @@
-
-
 insert into users(username, password, enabled) values ('admin', 'admin123!', true);
 insert into authorities(username, authority) values ('admin', 'ROLE_ADMIN');
 insert into users(username, password, enabled) values ('guest', 'guest', true);
